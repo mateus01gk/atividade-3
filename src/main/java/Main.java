430307569855ccd50c61f30ebf6e5ec844547feb
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-09-02 12:01:53 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("");
+    Scanner sc = new Scanner(System.in);
+    double Álcool = 1;
+    double gasolina = 2;
+    double diesel = 3;
+    double fim = 4;
+    System.out.println("o que você deseja hoje ");
+
+    sc.close();
+  }
+
+}
